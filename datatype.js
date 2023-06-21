7fadaa49de7e57cabb7c5de68aff26b1be8f5691
Vidhi <vidhikapopara404040@gmail.com>
2023-06-21 14:41:13 +0530

    datatype file update

---
+++ datatype.js
@@ -0,0 +1,19 @@
+console.log("vidhi")
+console.log(10)
+
+
+//variable name not start with the number
+//used aphabetls, $, _ only this in variable name
+//not used resived key word 
+
+fiestname="vidhi"
+name="isha"
+firstname="riya"
+console.log(fiestname)
+
+$first="yes"
+_next="yesss"
+fist&second="no"//not used any symbol
+new="nooo"//not used js keywords
+function="noo"
+_function="yess"
